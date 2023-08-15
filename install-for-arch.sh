c179e3fce70e18849cdf99ed43b0c4a9a05d01d4
FyrN <118836819+FyrN22@users.noreply.github.com>
2023-08-15 17:31:23 +0000

    Update install-for-arch.sh

--- install-for-arch.sh
+++ install-for-arch.sh
@@ -2,10 +2,14 @@ echo "This script will install you dwm, dmenu and slstatus"
 sleep 2s 
 clear
 echo "Installing depencies"
-sudo pacman -S kitty base-devel nitrogen ttf-iosevka-nerd xorg
-sudo make clean install dwm/
+sudo pacman -S kitty base-devel nitrogen ttf-iosevka-nerd xorg xorg-xinit
+cd dwm/
+sudo make clean install
+cd dmenu/
 sudo make clean install dmenu/
+cd slstatus/
 sudo make clean install slstatus/
+
 echo "slstatus &" >> ~/.xinitrc
 echo "nitrogen &" >> ~/.xinitrc 
 echo "exec dwm" >> ~/.xinitrc
